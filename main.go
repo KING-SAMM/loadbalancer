c1c02d58288b01c87436937e456a1fdd71701144
kcsamm <kcsamm11@gmail.com>
2022-12-18 00:20:41 +0100

    Added simpleServer struct
    Implemented newSimpleServer function
    
    Implemented handleErr function

--- main.go
+++ main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"net/http/httputil"
+	"net/url"
 )
 
 type simpleServer struct {
-	
+	addr string
+	proxy *httputil.ReverseProxy
+}
+
+func newSimpleServer(addr string) *simpleServer {
+	serverUrl, err := url.Parse(addr)
+
+	handleErr(err)
+
+	return &simpleServer{
+		addr: addr,
+		proxy: httputil.NewSingleHostReverseProxy(serverUrl),	
+	}
+}
+
+func handleErr(err error) {
+	if err != nil {
+		fmt.Printf("Error: %v \n", err)
+	}
 }
