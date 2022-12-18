1777c445b36f098dd83ed03c0de36868a1163717
kcsamm <kcsamm11@gmail.com>
2022-12-18 15:54:06 +0100

    Added Server interface, LoadBalancer struct
    Added NewLoadBalancer function
    
    Added getNextAvailableServer() and serverProxy() methods

--- main.go
+++ main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
 )
+
+type Server interface {
+	Address() string
+	IsAlive() bool
+	Serve(rw http.ResponseWriter, r *http.Request)
+}
 
 type simpleServer struct {
 	addr string
@@ -22,8 +30,29 @@ func newSimpleServer(addr string) *simpleServer {
 	}
 }
 
+type LoadBalancer struct {
+	port			string
+	roundRobinCount	int
+	servers 		[]Server
+}
+
+func NewLoadBalancer(port string, servers []Server) *LoadBalancer {
+	return &LoadBalancer{
+		port: port,
+		roundRobinCount: 0,
+		servers: servers,
+	}
+}
+
 func handleErr(err error) {
 	if err != nil {
 		fmt.Printf("Error: %v \n", err)
+		os.Exit(1)
 	}
 }
+
+func (lb *LoadBalancer) getNextAvailableServer() *Server {
+
+}
+
+func (lb *LoadBalancer) serveProxy(rw http.ResponseWriter, r http.Request ) {}
